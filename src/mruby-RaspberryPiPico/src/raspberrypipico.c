ff1ba5d8f4608498e5abbfc42e09b4e20dcea656
mimaki <hiroshi.mimaki@gmail.com>
2022-12-20 00:03:16 +0900

    GPIO library trial. (worked)

--- src/mruby-RaspberryPiPico/src/raspberrypipico.c
+++ src/mruby-RaspberryPiPico/src/raspberrypipico.c
@@ -4,6 +4,7 @@
 #include "mruby/value.h"
 #include "mruby/variable.h"
 
+#include "pico/stdlib.h"
 #include "pico/time.h"
 
 static mrb_value
@@ -24,8 +25,6 @@ mrb_time_usleep(mrb_state *mrb, mrb_value self)
   return mrb_nil_value();
 }
 
-#define GPIO_OUT  1
-#define GPIO_IN   2
 static mrb_value
 mrb_gpio_init(mrb_state *mrb, mrb_value self)
 {
@@ -33,31 +32,38 @@ mrb_gpio_init(mrb_state *mrb, mrb_value self)
   mrb_get_args(mrb, "i|i", &pin, &mode);
   mrb_iv_set(mrb, self, mrb_intern_lit(mrb, "@pin"), mrb_fixnum_value(pin));
   mrb_iv_set(mrb, self, mrb_intern_lit(mrb, "@mode"), mrb_fixnum_value(mode));
+  gpio_init(pin);
+  gpio_set_dir(pin, mode);
   return self;
 }
 static mrb_value
 mrb_gpio_setmode(mrb_state *mrb, mrb_value self)
 {
   mrb_int mode;
+  mrb_value opin = mrb_iv_get(mrb, self, mrb_intern_lit(mrb, "@pin"));
   mrb_get_args(mrb, "i", &mode);
   mrb_iv_set(mrb, self, mrb_intern_lit(mrb, "@mode"), mrb_fixnum_value(mode));
-  return self;
+  if (mode != GPIO_OUT && mode != GPIO_IN) {
+    // TODO: mode error
+    //
+  }
+  gpio_set_dir(mrb_integer(opin), mode);
+  return mrb_nil_value();
 }
 static mrb_value
 mrb_gpio_write(mrb_state *mrb, mrb_value self)
 {
   mrb_int val;
-  mrb_value opin, omode;
+  mrb_value opin = mrb_iv_get(mrb, self, mrb_intern_lit(mrb, "@pin"));
+  mrb_value omode = mrb_iv_get(mrb, self, mrb_intern_lit(mrb, "@mode"));
   mrb_get_args(mrb, "i", &val);
-  opin = mrb_iv_get(mrb, self, mrb_intern_lit(mrb, "@pin"));
-  omode = mrb_iv_get(mrb, self, mrb_intern_lit(mrb, "@mode"));
-  if ((mrb_integer(omode) & GPIO_OUT) == 0) {
+  if (mrb_integer(omode) != GPIO_OUT) {
     // TODO: mode error
     //
     return mrb_nil_value();
   }
-  // TODO: write value to GPIO
-  //
+  // write value to GPIO
+  gpio_put(mrb_integer(opin),val);
   return mrb_nil_value();
 }
 static mrb_value
@@ -67,15 +73,19 @@ mrb_gpio_read(mrb_state *mrb, mrb_value self)
   mrb_value opin, omode;
   opin = mrb_iv_get(mrb, self, mrb_intern_lit(mrb, "@pin"));
   omode = mrb_iv_get(mrb, self, mrb_intern_lit(mrb, "@mode"));
-  if ((mrb_integer(omode) & GPIO_IN) == 0) {
+  if (mrb_integer(omode) != GPIO_IN) {
     // TODO: mode error
     //
     return mrb_fixnum_value(val);
   }
-  // TODO: read value from GPIO
-  //
+  // read value from GPIO
+  val = gpio_get(mrb_integer(opin));
   return mrb_fixnum_value(val);
 }
+
+const uint8_t hoge[] = {
+  0
+};
 
 void
 mrb_mruby_raspberrypipico_gem_init(mrb_state *mrb)
@@ -85,14 +95,19 @@ mrb_mruby_raspberrypipico_gem_init(mrb_state *mrb)
   time = mrb_define_module_under(mrb, rasp, "Time");
   mrb_define_class_method(mrb, time, "msleep", mrb_time_msleep, MRB_ARGS_REQ(1));
   mrb_define_class_method(mrb, time, "usleep", mrb_time_usleep, MRB_ARGS_REQ(1));
+
   {
     struct RClass *gpio = mrb_define_class(mrb, "GPIO", mrb->object_class);
-    mrb_const_set(mrb, mrb_obj_value(gpio), mrb_intern_lit(mrb, "OUT"), mrb_fixnum_value(1));
-    mrb_const_set(mrb, mrb_obj_value(gpio), mrb_intern_lit(mrb, "IN"), mrb_fixnum_value(2));
+    mrb_const_set(mrb, mrb_obj_value(gpio), mrb_intern_lit(mrb, "OUT"), mrb_fixnum_value(GPIO_OUT));
+    mrb_const_set(mrb, mrb_obj_value(gpio), mrb_intern_lit(mrb, "IN"), mrb_fixnum_value(GPIO_IN));
     mrb_define_method(mrb, gpio, "initialize", mrb_gpio_init, MRB_ARGS_ARG(1, 1));
     mrb_define_method(mrb, gpio, "setmode", mrb_gpio_setmode, MRB_ARGS_REQ(1));
     mrb_define_method(mrb, gpio, "write", mrb_gpio_write, MRB_ARGS_REQ(1));
     mrb_define_method(mrb, gpio, "read", mrb_gpio_read, MRB_ARGS_NONE());
+    // for test
+    printf("hoge addr: %p\n", hoge);
+    uint8_t *mrbarea = (uint8_t*)0x10100000;
+    printf("mrbarea: %x %x %x %x\n", mrbarea[0], mrbarea[1], mrbarea[2], mrbarea[3]);
   }
 }
 
